b328faf240b790c920b014f8704f7af45eef05a5
denis <9172885049@mail.ru>
2019-07-24 11:56:05 +0300

    Lesson3

--- objectiveC-Lesson/main.m
+++ objectiveC-Lesson/main.m
@@ -77,7 +77,7 @@ void multiplication () {
     
     if ([actionOperation isEqualToString:@"*"]) {
         result = first * second;
-        NSLog(@"При вычитание получилось число %f", result);
+        NSLog(@"При умножение получилось число %f", result);
     } else {
         NSLog(@"Вы вели не правильную операцию");
     }
@@ -123,16 +123,93 @@ void alphabet () {
     }
 
 }
+// практическое задание на 3 урок от методички
+//--------------------------------------------------------------------------------------
+
+//1 задание
+void autoMarka () {
+    NSArray* marka = @[@"Audi", @"BMV", @"Mercedec", @"Kia", @"Nissan", @"Honda"];
+    
+    for (NSString* string in marka) {
+        NSLog(@"Маркa автомобилий: %@\n", string);
+    }
+}
+//2 задание
+enum OperationCal {
+    operationbuild,
+    operationsubtraction,
+    operationmultiplication,
+    operationdivision
+};
+typedef enum OperationCal OperationCal;
+
+float calculator(int a, OperationCal oper, int b ) {
+    float result = 0.0f;
+    
+    switch (oper) {
+        case operationbuild:
+            return a + b;
+            break;
+        case operationdivision:
+            return a / b;
+            break;
+        case operationsubtraction:
+            return a - b;
+            break;
+        case operationmultiplication:
+            return a * b;
+            break;
+        default:
+            break;
+    }
+}
+
+//3 задание
+enum Body {
+    standart,
+    sport,
+    fat
+};
+typedef enum Body Body;
+
+struct Human {
+    NSString *name;
+    NSInteger age;
+    Body body;
+};
+typedef struct Human Human;
+
+
+
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         
+        //1 задание
+        autoMarka();
+        //2 задание
+        int a = calculator(2, operationmultiplication, 2);
+        int b = calculator(10, operationdivision, 2);
+        NSLog(@"Result: \n a = %i, \n b = %i", a, b);
+        //3 задание
+        Human human1;
+        Human human2;
+        Human human3;
+        human1.name = @"Vasy";
+        human1.age = 31;
+        human2.name = @"Pety";
+        human2.age = 22;
+        human3.name = @"adel";
+        human3.age = 34;
         
-        build();
-        subtraction ();
-        multiplication ();
-        division ();
-        alphabet ();
+        NSLog(@"\n human1 name: %@, and age: %d, \n human2 name: %@ and age: %d, \n human3 name: %@ and age: %d" , human1.name, human1.age, human2.name, human2.age, human3.name, human3.age);
+        
+
+//        build();
+//        subtraction ();
+//        multiplication ();
+//        division ();
+//        alphabet ();
 
         
     }
